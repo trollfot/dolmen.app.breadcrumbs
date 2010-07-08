51d5b1cef8bc26ebef7834964dc807b3997ee1de
Souheil CHELFOUH <trollfot@gmail.com>
2010-07-08 12:23:32 +0200

    Added release notes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os.path import join
 
 name = 'dolmen.app.breadcrumbs'
-version = '0.2'
+version = '1.0b1'
 readme = open(join('src', 'dolmen', 'app', 'breadcrumbs', 'README.txt')).read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
